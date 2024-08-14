4f9afb5c10e1217c2e81d3aa127b9b5656f12037
gabivdcamargo <159277563+gabivdcamargo@users.noreply.github.com>
2024-08-14 16:41:09 -0300

    Primeiro Commit

---
+++ objetosPessoa.java
@@ -0,0 +1,38 @@
+package POO;
+public class objetosPessoa {
+
+	public static void main(String[] args) {
+		//Criando objetos da classe Pessoa
+		Pessoa pessoa1 = new Pessoa();
+		Pessoa pessoa2 = new Pessoa();
+
+		//OBJETO
+		pessoa1.setNome("Tom Cruise");
+		pessoa1.setIdade(60);
+		pessoa1.setEndereco("California, USA");
+		pessoa1.setProfissao("Ator");
+
+		//OBJETO 2
+		pessoa2.setNome("Messi");
+		pessoa2.setIdade(35);
+		pessoa2.setEndereco("Miami, USA");
+		pessoa2.setProfissao("Jogador de Futebol");
+
+		//Chamando o método get() para apresentar os objetos
+		System.out.println("----- OBJETO 1-----");
+		System.out.println(pessoa1.getNome());
+		System.out.println(pessoa1.getIdade());
+		System.out.println(pessoa1.getEndereco());
+		System.out.println(pessoa1.getProfissao());
+
+		System.out.println("----- OBJETO 2-----");
+		System.out.println(pessoa2.getNome());
+		System.out.println(pessoa2.getIdade());
+		System.out.println(pessoa2.getEndereco());
+		System.out.println(pessoa2.getProfissao());
+
+
+
+	}
+
+}
